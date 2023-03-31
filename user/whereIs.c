8ba9f7691ed32f4b518a1444ef8fe7bbf1f055e6
Franciscob1218 <Francsicob1218@gmail.com>
2023-03-31 16:18:20 -0500

    changed and commented out other version

--- user/whereIs.c
+++ user/whereIs.c
@@ -2,6 +2,32 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+#define ADDR_VALID (void*)0x4000
+#define ADDR_INVALID (void*)0x80000000
+
+int main(int argc, char *argv[]) {
+    // Call whereIs() with a valid virtual address
+    int frame_num = whereIs(ADDR_VALID);
+    if (frame_num >= 0) {
+        fprintf(1, "Physical frame number of %p: %d\n", ADDR_VALID, frame_num);
+    } else {
+        fprintf(1, "Error: virtual address %p is invalid\n", ADDR_VALID);
+    }
+
+    // Call whereIs() with an invalid virtual address
+    frame_num = whereIs(ADDR_INVALID);
+    if (frame_num >= 0) {
+        fprintf(1, "Physical frame number of %p: %d\n", ADDR_INVALID, frame_num);
+    } else {
+        fprintf(1, "Error: virtual address %p is invalid\n", ADDR_INVALID);
+    }
+
+    exit(0);
+}
+
+/*
+
 #define PAGE_SIZE 4096
 
 int main(int argc, char *argv[])
@@ -30,3 +56,7 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+
+
+*/
