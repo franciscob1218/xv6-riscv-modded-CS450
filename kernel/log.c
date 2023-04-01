d5ac283a8ffd51956c990b513e757be00b9f19f9
Franciscob1218 <Francsicob1218@gmail.com>
2023-03-31 23:10:37 -0500

    fixed log.c to old version

--- kernel/log.c
+++ kernel/log.c
@@ -6,6 +6,7 @@
 #include "sleeplock.h"
 #include "fs.h"
 #include "buf.h"
+
 // Simple logging that allows concurrent FS system calls.
 //
 // A log transaction contains the updates of multiple FS system
