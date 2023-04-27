e18134a5e5ba64da1a174691a4f453d98278ec7b
dei <dgilys@hawk.iit.edu>
2023-04-27 01:38:19 -0500

    headers for dirErase

--- user/dirErase.c
+++ user/dirErase.c
@@ -1,8 +1,8 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "kernel/fs.h"
-#include "user/user.h"
-#include "kernel/fcntl.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../kernel/fs.h"
+#include "user.h"
+#include "../kernel/fcntl.h"
 
 #define MAX_PATH_LENGTH 256
 
