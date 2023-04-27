2b9a394a7da11f40b2d5d2e9f04a4961abc37711
dei <dgilys@hawk.iit.edu>
2023-04-27 00:36:49 -0500

    changed headers for inodeReader

--- user/inodeReader.c
+++ user/inodeReader.c
@@ -1,8 +1,8 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "kernel/fs.h"
-#include "user/user.h"
-#include "kernel/fcntl.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../kernel/fs.h"
+#include "user.h"
+#include "../kernel/fcntl.h"
 
 // Helper function to construct a path for the inode
 void inode_path(char *buf, int size, int inum) {
